6d1e7d1f6125d3ca150c3a1fdf8162be33247055
lukastk <lukas.kikuchi@gmail.com>
2019-02-23 14:00:57 +0000

    Search stuff and autocomplete

---
+++ static/datatree.js
@@ -0,0 +1,64 @@
+sectors_tree = {
+  "Education" : {
+    "High school" : {},
+    "Primary school" : {},
+    "University" : {}
+  },
+  "Entertainment" : {
+    "TV" : {},
+    "Film" : {}
+  },
+};
+
+countries_tree = {
+  "Africa" : {
+    "Nigera" : {},
+    "Zimbabwe" : {}
+  },
+  "Europe" : {
+    "Sweden" : {},
+    "United Kingdom" : {
+      "Wales" : {},
+      "England" : {},
+      "Scotland" : {},
+      "Northern Ireland" : {}
+    }
+  },
+  "North America" : {
+    "USA" : {}
+  }
+};
+
+sectors_children = {};
+countries_children = {};
+
+sectors_all = []
+countries_all = []
+
+var find_children = function(node, parents, children) {
+  for (var nk in node) {
+
+    for (var i = 0; i < parents.length; i++) {
+      children[parents[i]].push(nk)
+    }
+
+    nd = node[nk];
+    children[nk] = [];
+    parents2 = parents.slice();
+    parents2.push(nk);
+    find_children(nd, parents2, children)
+  }
+}
+
+var find_all_elements = function(node, elemlist) {
+  for (var nk in node) {
+    elemlist.push(nk)
+    find_all_elements(node[nk], elemlist)
+  }
+}
+
+find_children(sectors_tree, [], sectors_children);
+find_children(countries_tree, [], countries_children);
+
+find_all_elements(sectors_tree, sectors_all);
+find_all_elements(countries_tree, countries_all);
--- static/scripts.js
+++ static/scripts.js
@@ -24,14 +24,43 @@ var sweep = function(search_words, col="") {
   });
 }
 
+var fund_sweep = function(currency, fmin, fmax) {
+  console.log(123)
+  $("#investors tbody tr").each(function() {
+    try {
+      var hasKey = false;
+
+      cols = $(this).find(".fund-data")
+
+      row_currency = cols.attr("data-fundc");
+      row_min = parseInt(cols.attr("data-fundmin"));
+      row_max = parseInt(cols.attr("data-fundmax"));
+
+      if (currency === row_currency) {
+        if ( row_max >= fmin && row_min <= fmax) {
+          hasKey = true;
+        }
+      }
+
+      if (hasKey) {
+        $(this).css("display","");
+      } else {
+        $(this).css("display","none");
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  });
+}
+
 var search = function() {
   $("#investors tbody tr").each(function() {
     $(this).css("display","");
   });
 
   if ($("#keywords-search").val() !== "") {
-    keywords = $("#keywords-search").val().split(/[ ,]+/);
-    sweep(keywords);
+    col_search = $("#keywords-search").val().split(/[ ,]+/);
+    sweep(col_search);
   }
 
   var col = "organisation";
@@ -40,10 +69,51 @@ var search = function() {
     sweep(col_search, col);
   }
 
-  var col = "region";
+  var col = "sector";
   if ($("#"+col+"-search").val() !== "") {
     col_search = $("#"+col+"-search").val().split(/[ ,]+/);
     sweep(col_search, col);
+  }
+
+  var col = "type";
+  if ($("#"+col+"-search").val() !== "") {
+    col_search = $("#"+col+"-search").val().split(/[ ,]+/);
+    sweep(col_search, col);
+  }
+
+  var col = "country";
+  if ($("#"+col+"-search").val() !== "") {
+    col_search = $("#"+col+"-search").val().match(/\\?.|^$/g).reduce((p, c) => {
+            if(c === '"'){
+                p.quote ^= 1;
+            }else if(!p.quote && c === ' '){
+                p.a.push('');
+            }else{
+                p.a[p.a.length-1] += c.replace(/\\(.)/,"$1");
+            }
+            return  p;
+        }, {a: ['']}).a;
+
+    add = [];
+    for (var i = 0; i < col_search.length; i++) {
+      if (col_search[i] in countries_children) {
+        add = add.concat(countries_children[col_search[i]]);
+      }
+    }
+
+    col_search = col_search.concat(add);
+    sweep(col_search, col);
+  }
+
+  try {
+    if ($("#fund-currency-search").val() !== "" &&  $("#fund-size_min-search").val() !== "" && $("#fund-size_max-search").val() !== "") {
+      currency = $("#fund-currency-search").val();
+      fmin = parseInt($("#fund-size_min-search").val());
+      fmax = parseInt($("#fund-size_max-search").val());
+      fund_sweep(currency, fmin, fmax);
+    }
+  } catch (err) {
+      console.log(err);
   }
 }
 
